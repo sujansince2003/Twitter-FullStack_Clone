b575f27e4d8534ba87fe9ae3ea3f8efc9cb50c07
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-12 20:20:09 +0545

    build model option to close when click on outside

--- client/app/Components/Feedcard.tsx
+++ client/app/Components/Feedcard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, MutableRefObject } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaRegBookmark } from "react-icons/fa6";
@@ -8,9 +8,14 @@ import { IoMdHeartEmpty, IoMdHeart, IoMdStats } from "react-icons/io";
 import { FiShare } from "react-icons/fi";
 import { FaBookmark } from "react-icons/fa6";
 import { MdMoreHoriz } from "react-icons/md";
+import { useClickAway } from "@uidotdev/usehooks";
 
 const Feedcard = () => {
   const [isClick, setIsClick] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useClickAway(() => {
+    setIsOpen(false);
+  }) as MutableRefObject<HTMLDivElement>;
   return (
     <div className="border-t border-gray-200 p-4 hover:bg-slate-50 ">
       <div className="grid gap-2 grid-cols-12">
@@ -23,7 +28,7 @@ const Feedcard = () => {
             className="rounded-full "
           />
         </div>
-        <div className="col-span-11 flex gap-2 flex-col">
+        <div className="col-span-11 flex gap-2 flex-col border-2 border-red-200 relative ">
           <div className="flex items-center justify-between">
             <div className="flex gap-1 ">
               <Link href={"/user"} className="font-semibold ">
@@ -33,9 +38,21 @@ const Feedcard = () => {
               <span className="text-slate-500">·</span>
               <h4 className="text-slate-500">10 july</h4>
             </div>
-            <button className="p-2 hover:bg-blue-50  rounded-full ">
+            <button
+              onClick={() => setIsOpen((prev) => !prev)}
+              className="p-2 hover:bg-blue-50  rounded-full "
+            >
               <MdMoreHoriz className="text-xl" />
             </button>
+            {isOpen && (
+              <div ref={ref} className="absolute top-2 right-0 ">
+                <div className="w-32 h-22 border-2 border-slate-600 rounded-2xl bg-violet-300 p-2">
+                  <h1>Folloe</h1>
+                  <h1>Folloe</h1>
+                  <h1>Folloe</h1>
+                </div>
+              </div>
+            )}
           </div>
           <p>
             I still remember how Virat Kohli led India to a victory in the

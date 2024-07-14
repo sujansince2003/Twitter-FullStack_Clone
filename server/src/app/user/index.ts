a6d609cbae29b1c1a62777add7fcc1fbd6a7ae71
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-14 09:45:01 +0545

    defined types,resolvers,queries and mututations for user

---
+++ server/src/app/user/index.ts
@@ -0,0 +1,9 @@
+import { queries } from "./queries";
+import { resolvers } from "./resolvers";
+import { types } from "./types";
+queries
+resolvers
+
+types
+
+export const User = { queries, resolvers, types }
---
+++ server/src/app/user/types.ts
@@ -0,0 +1,17 @@
+export const types = `#graphql
+
+type User{
+id:ID,
+fname:String!,
+lname:String,
+email:String!,
+profileImg:String,
+
+
+
+
+}
+
+
+
+`

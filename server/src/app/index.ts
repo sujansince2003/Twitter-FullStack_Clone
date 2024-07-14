1c5e167333489210f7473090a42d0d19c06adb21
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-14 09:44:12 +0545

    added type,query and resolvers to graphql server

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -2,20 +2,27 @@ import express, { query } from "express"
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from "@apollo/server/express4"
 import bodyParser from "body-parser"
+import { prismaclient } from "../client/db"
+import { User } from "./user"
+
 
 export async function initGraphqlServer() {
     const app = express()
 
     app.use(bodyParser.json())
+
+
+
     const graphqlServer = new ApolloServer({
         typeDefs: `
+        ${User.types}
         type Query {
-        say:String
+       ${User.queries}
         }
         `,
         resolvers: {
             Query: {
-                say: () => "Hello graphql"
+                ...User.resolvers.queries,
             },
 
 

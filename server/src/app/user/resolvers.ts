aba27fe48aca3351a1f0de5c9e4f6d0d64782b90
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-17 23:56:08 +0545

    resolver function to handle auth token from google

--- server/src/app/user/resolvers.ts
+++ server/src/app/user/resolvers.ts
@@ -1,8 +1,58 @@
+import axios from "axios";
+import { prismaclient } from "../../client/db";
+import { User } from "@prisma/client";
+interface GoogleUserInfo {
+    iss?: string;
+    sub?: string;
+    azp?: string;
+    aud?: string;
+    iat?: string;
+    exp?: string;
+    email?: string;
+    email_verified?: string;
+    name?: string;
+    picture?: string;
+    given_name?: string;
+    family_name?: string;
+    locale?: string;
+}
+
 export const queries = {
-
-
     verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-        return token;
+        // return token
+        const googleToken = token;
+        const googleAuthUrl = new URL("https://oauth2.googleapis.com/tokeninfo")
+        googleAuthUrl.searchParams.set("id_token", googleToken)
+
+        const { data } = await axios.get<GoogleUserInfo>(googleAuthUrl.toString(), {
+            responseType: "json"
+        })
+        console.log(data)
+        const isUserAlreadyAvailable = await prismaclient.user.findUnique({
+            where:
+            {
+                email: data.email
+            }
+        })
+        console.log(isUserAlreadyAvailable);
+
+        if (!isUserAlreadyAvailable) {
+            const createUser = await prismaclient.user.create({
+                data:
+                {
+                    email: data.email!,
+                    fname: data.given_name,
+                    profileImg: data.picture!,
+
+
+
+                }
+            })
+        }
+
+
+        console.log("ysers is", isUserAlreadyAvailable);
+        return ("hello")
     }
 
 
